41e08a03120f447d909035bfb4a33c58428db6f5
jfriend08 <ys486@cornell.edu>
2015-09-16 09:15:53 -0400

    add comments, and check correctness

--- problem1/exact.py
+++ problem1/exact.py
@@ -4,11 +4,11 @@ import sys
 class exactChange(object):
   def __init__(self, N):
     self.N = N
+    self.maxChange = 239
 
-  def getExactChange(self, changeAmount):
-    diffChanges = [25, 10, 5, 1] #quatar, dime, nickel, pennie
+  def getExactChange(self, changeAmount, Denoms):
     result = []
-    for eachChange in diffChanges:
+    for eachChange in Denoms:
       numChange = int(changeAmount/eachChange)
       if changeAmount <= 0:
         result.append(0)
@@ -18,11 +18,11 @@ class exactChange(object):
     return result
 
   def getScoreWithGivenDenominations(self, Denoms):
-    maxChange = 239
+    # input denomination list, then calculate changeArray, and then return the score
     if not Denoms:
       return
-    changeArray = [0 for i in range(maxChange)]
 
+    changeArray = [0 for i in range(self.maxChange)]
     for idx in xrange(len(changeArray)):
       changeAmount = idx + 1
       bestExactChange = sys.maxint
@@ -30,6 +30,7 @@ class exactChange(object):
         changeArray[idx] = 1
       else:
         for eachDenom in Denoms:
+          # always need to add up one coin. dynamic programmingly check previous results and find the best one
           diff = changeAmount - eachDenom
           if diff > 0:
             bestExactChange = min(bestExactChange, changeArray[diff-1] + 1)
@@ -44,4 +45,5 @@ class exactChange(object):
 if __name__ == "__main__":
   arg = sys.argv[1]
   solution = exactChange(float(arg))
-  solution.getScoreWithGivenDenominations([1])
+  print solution.getExactChange(238,[25,10,5,1]) ## for checking
+  solution.getScoreWithGivenDenominations([1,5,10,25])
