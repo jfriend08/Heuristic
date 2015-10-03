fcaaee89c4f176b9c9a7284fa18328ed6c1f519a
jfriend08 <ys486@cornell.edu>
2015-10-03 16:45:41 -0400

    being able to connect port and send

---
+++ problem3/tipping.cc
@@ -0,0 +1,271 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <utility>
+#include <math.h>
+#include <cmath>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+#define PORT "8013"  // the port users will be connecting to
+
+#define BACKLOG 1   // how many pending connections queue will hold
+
+void sigchld_handler(int s)
+{
+  // waitpid() might overwrite errno, so we save and restore it:
+  int saved_errno = errno;
+
+  while(waitpid(-1, NULL, WNOHANG) > 0);
+
+  errno = saved_errno;
+}
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+// ---------- No tipping game algorithm ----------------
+string recvCmd(int fd) {
+  string ret("");
+
+  while (ret.find("STATE END") == string::npos) {
+    char buff[100];
+    if (recv(fd, (void*) buff, 100, 0) == -1) {
+      perror("Recv Error\n");
+    } else {
+      ret.append(buff);
+    }
+  }
+  ret.push_back('\0');
+
+  return ret;
+}
+
+struct Weight {
+  int position;
+  int weight;
+  bool mine;
+
+  Weight(int p, int w, bool m): position(p), weight(w), mine(m) {};
+  Weight(): position(0), weight(0), mine(false) {};
+};
+
+struct MoveOpt {
+  int position;
+  int weight;
+  float prob;
+
+  MoveOpt(int p, int w, float b): position(p), weight(w), prob(b) {};
+  MoveOpt(): position(0), weight(0), prob(0.f) {};
+};
+
+bool compProb(MoveOpt *a, MoveOpt *b) {
+  return a->prob < b->prob;
+}
+
+class NoTippingPlayer {
+private:
+  vector<Weight*> board;
+  set<int> availables;
+  int left_torq;
+  int right_torq;
+  bool firstPlayer;
+
+  int getIdx(int i) {
+    return 25 + i;
+  }
+
+  bool isTipping() {
+    return left_torq > 0 || right_torq < 0;
+  }
+
+  const char* addWeight() {
+    // vector<ModeOpt*> choices;
+    for (int i = 0; i < board.size(); i++) {
+      if (board[i]->weight) {
+        continue;
+      }
+
+      MoveOpt *now = new MoveOpt();
+
+    }
+    return "1 15";
+  }
+
+  const char* removeWeight() {
+    return "";
+  }
+
+public:
+  NoTippingPlayer() {
+    for (int i = -25; i <= 25; i++) {
+      board.push_back(new Weight(getIdx(i), 0, false));
+    }
+    board[getIdx(-3)]->weight = 3;
+    left_torq = 0;
+    right_torq = 0;
+    firstPlayer = false;
+    for (int i = 0; i < 15; availables.insert(i++)) ;
+  }
+
+  void setFirstPlayer() {
+    firstPlayer = true;
+    printf("setFirstPlayer\n");
+  }
+
+  const char* nextMove(bool adding, int position, int weight) {
+    //Update state
+    left_torq -= (position - (-3)) * weight;
+    right_torq -= (position - (-1)) * weight;
+
+    if (isTipping()) {
+      return "";
+    }
+
+    delete board[position];
+    board[position] = new Weight(position, weight, false);
+
+    return adding ? addWeight() : removeWeight();
+  }
+
+  string letMeThink(){
+    string result = "-1 15";
+
+    // return result.c_str();
+    return result;
+  }
+};
+
+// ------------ Main ------------------------------------
+
+int main(void)
+{
+  int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+  struct addrinfo hints, *servinfo, *p;
+  struct sockaddr_storage their_addr; // connector's address information
+  socklen_t sin_size;
+  struct sigaction sa;
+  int yes=1;
+  char s[INET6_ADDRSTRLEN];
+  int rv;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; // use my IP
+
+  if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+
+  // loop through all the results and bind to the first we can
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype,
+        p->ai_protocol)) == -1) {
+      perror("server: socket");
+      continue;
+    }
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+        sizeof(int)) == -1) {
+      perror("setsockopt");
+      exit(1);
+    }
+
+    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("server: bind");
+      continue;
+    }
+
+    break;
+  }
+
+  freeaddrinfo(servinfo); // all done with this structure
+
+  if (p == NULL)  {
+    fprintf(stderr, "server: failed to bind\n");
+    exit(1);
+  }
+
+  if (listen(sockfd, BACKLOG) == -1) {
+    perror("listen");
+    exit(1);
+  }
+
+  sa.sa_handler = sigchld_handler; // reap all dead processes
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
+  }
+
+  printf("server: waiting for connections...\n");
+
+  NoTippingPlayer player;
+
+  while(1) {  // main accept() loop
+    sin_size = sizeof their_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+    if (new_fd == -1) {
+      perror("accept");
+      continue;
+    }
+
+    inet_ntop(their_addr.ss_family,
+      get_in_addr((struct sockaddr *)&their_addr),
+      s, sizeof s);
+    printf("server: got connection from %s\n", s);
+
+    string buff = recvCmd(new_fd);
+    char cmd[10];
+    int pos, weight;
+    sscanf(buff.c_str(), "%s %d %d", cmd, &pos, &weight);
+    if (pos==0 && weight==0){
+      player.setFirstPlayer();
+    }
+    printf("cmd: %s\n", cmd);
+    printf("%s pos: %d, weight:%d\n", cmd, pos, weight);
+
+    string myNextMove = player.letMeThink();
+    //TODO: Entry point to player
+
+    if (!fork()) { // this is the child process
+      close(sockfd); // child doesn't need the listener
+      // printf("%s", myNextMove.c_str());
+      if (send(new_fd, myNextMove.c_str(), myNextMove.length(), 0) == -1)
+        perror("");
+      close(new_fd);
+      exit(0);
+    }
+    close(new_fd);  // parent doesn't need this
+  }
+
+  return 0;
+}
