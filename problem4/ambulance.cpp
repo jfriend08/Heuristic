49897c641b7c8c497aaa3cba139c3a77fa5bab0a
jfriend08 <ys486@cornell.edu>
2015-10-17 12:12:48 -0400

    cin input and save to struct

--- problem4/ambulance.cpp
+++ problem4/ambulance.cpp
@@ -24,9 +24,37 @@ class Solution {
 
 
 int main(int argc, char *argv[]){
-  int num_Patients=50, num_Hospital=5;
-  int tmp_string;
+  vector<int> ambulance;
+  vector<patientInfo> allPatients;
+
+  char separator;
+  int num_Patients = 50, num_Hospital = 5, num_Ambulance, count = 0;
+  string tmp_string;
   int xloc,yloc,rescuetime;
+
+  cin.ignore(100000, '(');
+  cin >> tmp_string;
+
+  while (count < num_Patients) {
+    cin >> xloc >> separator >> yloc >> separator >> rescuetime;
+    patientInfo myPatient(xloc, yloc, rescuetime);
+    allPatients.push_back(myPatient);
+    count++;
+  }
+
+  cin >> tmp_string;
+  count = 0;
+  while (count < num_Hospital) {
+    cin >> num_Ambulance;
+    printf ("num_Ambulance: %d\n", num_Ambulance);
+    ambulance.push_back(num_Ambulance);
+    count++;
+  }
+
+  for (int i=0; i<allPatients.size(); i++){
+    printf ("%d\t%d\t%d\n", allPatients[i].xloc, allPatients[i].yloc, allPatients[i].rescuetime);
+  }
+
 
 }
 
