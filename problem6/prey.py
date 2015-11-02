28622e53a8a5c04424ffc29f1d75a65df86dfee4
jfriend08 <ys486@cornell.edu>
2015-11-01 23:43:47 -0500

    current version

--- problem6/prey.py
+++ problem6/prey.py
@@ -29,18 +29,34 @@ class Prey(object):
     result = json.loads(mainSocket.recv())
     print "recvPublisher", result
 
+  def checkWalls(self):
+    return None
+  def decideMove(self):
+    return None
 
 def main():
   myPrey = Prey()
 
   stepCount = 1
   while(stepCount < 10):
+    print "stepCount", stepCount
     if stepCount%2 == 1:
+      #in this round, prey do nothing
       socketH.send(json.dumps({"command":"M", "direction": "S"}))
       myPrey.recvPublisher()
     elif stepCount%2 == 0:
-      myPrey.addMove("NE")
+      #check states
       myPrey.checkPosition()
+      myPrey.checkWalls()
+
+      #prey decide moves, and make move
+      nextMove = myPrey.decideMove()
+      myPrey.addMove("NE")
+
+      #hunter will make move
+      socketH.send(json.dumps({"command":"M", "direction": "S"}))
+
+      myPrey.recvPublisher()
 
     stepCount += 1
 
