47b0230c705d3bb02ca9df4ba5fbdf28b4298447
jfriend08 <ys486@cornell.edu>
2015-11-02 09:32:28 -0500

    consider H direction to get relative direction, and know whether prey is at the back

--- problem6/prey.py
+++ problem6/prey.py
@@ -14,6 +14,7 @@ class Prey(object):
     self.walls = [];
     self.publisherMsg = False;
     self.allDirs = {"0_-1":"N", "0_1":"S", "1_0":"E", "-1_0":"W", "1_-1":"NE", "-1_-1":"NW", "1_1":"SE", "-1_1":"SW"}
+    self.Dir2Coordinate = {"N":(0.-1), "S":(0,1), "E":(1,0), "W":(-1,0), "NE":(1,-1), "NW":(-1,-1), "SE":(1,1), "SW":(-1,1)}
 
   def checkPosition(self):
     data = {"command":"P"}
@@ -21,10 +22,18 @@ class Prey(object):
     result = json.loads(socketP.recv())
     self.preyPos = result["prey"]
     self.hunterPos = result["hunter"]
-    # print "self.preyPos", self.preyPos
 
   def addMove(self, direction):
     socketP.send(json.dumps({"command":"M", "direction": direction}))
+
+  def getDriection(self, HPos0, PPos1):
+    if len(HPos0)==0 or len(PPos1)==0:
+      return
+    dx = (1 if PPos1[0] - HPos0[0] > 0 else PPos1[0] - HPos0[0])
+    dx = (-1 if PPos1[0] - HPos0[0] < 0 else dx)
+    dy = (1 if PPos1[1] - HPos0[1] > 0 else PPos1[1] - HPos0[1])
+    dy = (-1 if PPos1[1] - HPos0[1] < 0 else dy)
+    return (dx,dy)
 
   def updateHDriection(self, HPos0, HPos1):
     if len(HPos0)==0 or len(HPos1)==0:
@@ -34,7 +43,7 @@ class Prey(object):
     dy = (1 if HPos1[1] - HPos0[1] > 0 else HPos1[1] - HPos0[1])
     dy = (-1 if HPos1[1] - HPos0[1] < 0 else dy)
     self.hunterDirection = self.allDirs[str(dx)+"_"+str(dy)]
-
+    print self.hunterDirection
 
   def recvPublisher(self):
     result = json.loads(mainSocket.recv())
@@ -44,11 +53,31 @@ class Prey(object):
     self.walls = result["walls"]
     print "preyPos", self.preyPos
     print "hunterPos", self.hunterPos
-    # print "recvPublisher", result
+
+  def preyAtBack(self):
+    #return 0 if can be close by both HorV walls
+    #return 1 if can be close by either HorV walls
+    #return 2 if cannot be closed by any walls
+    dir_P2H = self.getDriection(self.hunterPos, self.preyPos)
+    hunterDirection = self.Dir2Coordinate[self.hunterDirection]
+    print "dir_P2H", dir_P2H
+    print "hunterDirection", hunterDirection
+    product1 = hunterDirection[0]*dir_P2H[0]
+    product2 = hunterDirection[1]*dir_P2H[1]
+    if product1 >0 and product2 >0:
+      return 0
+    elif product1 <0 and product2 <0:
+      return 2
+    else:
+      return 1
+
+
 
   def checkWalls(self):
     return None
   def decideMove(self):
+    print "preyAtBack", self.preyAtBack()
+
     return None
 
 def main():
