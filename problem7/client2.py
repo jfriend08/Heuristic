bfa9d9d64328e423a38a77bcc05342dc78933adc
jfriend08 <ys486@cornell.edu>
2015-11-16 21:13:21 -0500

    move

--- problem7/client2.py
+++ problem7/client2.py
@@ -1,11 +1,32 @@
-import sys
+import sys, os
+import signal
 import random
 import time
 import argparse
-
 from twisted.internet import reactor, protocol
 
 board_size = 1000
+
+def signal_handler(signum, frame):
+    raise Exception("Timed out!")
+
+class TimedOutExc(Exception):
+  pass
+
+def deadline(timeout, *args):
+  def decorate(f):
+    def handler(signum, frame):
+      raise TimedOutExc()
+
+    def new_f(*args):
+      signal.signal(signal.SIGALRM, handler)
+      signal.alarm(timeout)
+      return f(*args)
+      signal.alarm(0)
+
+    new_f.__name__ = f.__name__
+    return new_f
+  return decorate
 
 class Client(protocol.Protocol):
   """Random Client"""
@@ -15,6 +36,8 @@ class Client(protocol.Protocol):
     self.nodeStates = {}
     self.nodeConCount = []
     self.playerStates = {}
+    self.Paths = []
+    self.receiveCount = 0
 
   def reset(self):
     print "Reset called"
@@ -50,15 +73,76 @@ class Client(protocol.Protocol):
     self.nodeConCount = [0 for i in xrange(len(self.nodeStates.keys()))]
     for key in self.nodeStates.keys():
       self.nodeConCount[int(key)] =  4 - "".join(self.nodeStates[key]['linkage']).count('null')
-      print self.nodeConCount[int(key)]
+    print "PARSE DONE"
 
+
+  def bfsFindPath(self, visitedIdx, startIdx, directions):
+    if self.nodeStates[startIdx]['state']!="FREE":
+      pass
+
+    linkList = self.nodeStates[startIdx]['linkage']
+    if "".join(linkList).count('null') == 3:
+      print "visitedIdx", visitedIdx, "startIdx", startIdx
+      visitedIdx.append(startIdx)
+      print "visitedIdx", visitedIdx, "startIdx", startIdx
+      self.Paths.append(visitedIdx)
+      print "self.Paths", self.Paths
+
+    for nodeid in linkList:
+      if nodeid != "null" and self.nodeStates[nodeid]['state']=="FREE" and nodeid not in visitedIdx:
+        vCopy = visitedIdx[:]
+        vCopy.append(startIdx)
+        self.bfsFindPath(vCopy, nodeid)
+
+  @deadline(1)
+  def bfsHandler(self, visitedIdx, startIdx):
+    self.bfsFindPath(visitedIdx, startIdx)
+
+  def bfsFindPath2(self, visitedIdx, startIdx, directions, depth):
+    print "visitedIdx", visitedIdx, "startIdx", startIdx, "directions", directions, "depth", depth
+    if startIdx == "null":
+      return
+    if self.nodeStates[startIdx]['state']!="FREE":
+      return
+
+    noMoveCount = 0
+    noValidCount = 0
+    linkList = self.nodeStates[startIdx]['linkage']
+    result = []
+    for idx in xrange(len(linkList)):
+      nodeid = linkList[idx]
+      if nodeid =="null" or nodeid in visitedIdx:
+        noValidCount+=1
+      if nodeid =="null" or nodeid in visitedIdx or idx in directions:
+        noMoveCount+=1
+        continue
+      if self.nodeStates[nodeid]['state']!="FREE":
+        continue
+      cpV = visitedIdx[:]
+      cpDir = directions[:]
+      cpV.append(startIdx)
+      cpDir.append(idx)
+      result.extend(self.bfsFindPath2(cpV, nodeid, cpDir, depth+1))
+    if noMoveCount==4:
+      return (visitedIdx, directions)
+    return result
+
+  def thinkMove(self):
+    self.Paths = []
+    for i in xrange(len(self.nodeConCount)):
+      count = self.nodeConCount[i]
+      curNodeIdx = str(i)
+      if count == 1:
+        print self.bfsFindPath2([], curNodeIdx, [], 0)
+        
 
 
   def dataReceived(self, data):
     self.parseStates(data)
-    myR = self.thinkMove
+    myR = self.thinkMove()
     myR = str(random.randint(0, 158)) + ",UP,DOWN,LEFT,RIGHT" + "\n"
     self.transport.write(myR)
+
 
   def connectionMade(self):
     self.transport.write('REGISTER: {0}\n'.format(self.name))
