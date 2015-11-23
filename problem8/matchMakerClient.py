a666783e9640aa8f9b1c2859d982145fb2f26ca5
jfriend08 <ys486@cornell.edu>
2015-11-22 21:36:57 -0500

    parse the first 20 report

--- problem8/matchMakerClient.py
+++ problem8/matchMakerClient.py
@@ -1,7 +1,6 @@
-import sys, os
+import sys, os, re
 import signal
 import random
-import time
 import argparse
 from twisted.internet import reactor, protocol
 import numpy as np
@@ -17,13 +16,19 @@ class TimedOutExc(Exception):
 
 class Client(protocol.Protocol):
   """Random Client"""
-  def __init__(self, name):
-    pass
+  def __init__(self, N):
+    self.N = int(N)
 
   def dataReceived(self, data):
-    print "data", data
-
-
+    print "data\n", data, "\n------------------------\n"
+    data = re.split('\n+', data)
+    if len(data) > 10:
+      data = map(lambda x:re.split('\s+|\|', x), data)
+      del data[-1]
+      m = np.array([elm[:self.N] for elm in data])
+      y = np.array([elm[self.N+2] for elm in data])
+      print "y.shape", y.shape
+      print y
 
   def connectionMade(self):
     # self.transport.write('REGISTER: {0}\n'.format(self.name))
@@ -34,11 +39,11 @@ class Client(protocol.Protocol):
 
 class ClientFactory(protocol.ClientFactory):
   """ClientFactory"""
-  def __init__(self, name):
-    self.name = name
+  def __init__(self, N):
+    self.N = N
 
   def buildProtocol(self, addr):
-    c = Client(self.name)
+    c = Client(self.N)
     c.addr = addr
     return c
 
@@ -51,51 +56,14 @@ class ClientFactory(protocol.ClientFactory):
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument("name", help="Your name")
+  parser.add_argument("N", help="N elements")
   parser.add_argument("-p", "--port", type=int, default="9696", help="Server port to connect to.")
   args = parser.parse_args()
-  client_name = args.name
+  N = args.N
   port = args.port
-  factory = ClientFactory(client_name)
+  factory = ClientFactory(N)
   reactor.connectTCP("localhost", port, factory)
   reactor.run()
 
 if __name__ == '__main__':
     main()
-
-
-
-
-# from websocket import create_connection
-# import json, socket
-
-# port = 9696
-# s = socket.socket()
-# s.connect(('127.0.0.1', port))
-
-# # mainSocket = create_connection('ws://localhost:127.0.0.1');
-# # socketM = create_connection('ws://localhost:9696');
-
-# class MatchMaker(object):
-#   def __init__(self):
-#     pass
-
-
-# def main():
-#   # hunter = Hunter(3, 5)
-#   # socketH.send(json.dumps({'command': 'M'}))
-
-#   gameover = False
-#   while gameover is False:
-#     cmd = json.loads(s.recv(1024))
-#     print "cmd", cmd
-#     # gameover = cmd['gameover']
-#     # if gameover is False:
-#     #   newMove = hunter.make_move(cmd)
-#     #   socketH.send(json.dumps(newMove))
-#     # else:
-#     #   print 'DONE: ' + str(cmd['time'])
-
-
-# if __name__ == '__main__':
-#   main()
